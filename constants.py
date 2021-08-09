b01c8e7c86f1af036b1bd8ce621dfddd06367bcc
MickelAngel0 <46452203+MickelAngel0@users.noreply.github.com>
2021-08-09 10:39:40 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -11,6 +11,6 @@ POST_HEADERS: dict = {"Content-Type": "application/json"}
 #: Format "http://127.0.0.1:5000/"
 peers: set = set()
 
-CONSENSOUS_AFTER_N_BLOCKS = 2
+CONSENSOUS_AFTER_N_BLOCKS = 5
 
-VERSION: float = 0.1
+VERSION: float = 0.2
