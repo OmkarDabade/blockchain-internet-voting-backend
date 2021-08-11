2fd07e393506ebcfe07ada24dad8d6f04ef3fa82
Omkar Dabade <od.omkard@gmail.com>
2021-08-11 21:46:26 +0530

    uncommented test data

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from blockchain import blockchain
 if __name__ == "__main__":
 
     # Test Data
-    # for i in range(5):
-    #     blockchain.addBlock(
-    #         100 + i,
-    #         f"Candidate Name %s" % (i + 1),
-    #         f"Voter Id %s" % (i + 1),
-    #     )
+    for i in range(5):
+        blockchain.addBlock(
+            100 + i,
+            f"Candidate Name %s" % (i + 1),
+            f"Voter Id %s" % (i + 1),
+        )
 
-    # for vote in blockchain.chain:
-    #     print(vote)
+    for vote in blockchain.chain:
+        print(vote)
 
     iVoteApp.run()
